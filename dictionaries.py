7a3ec6f8cce13d2ee51a794198a424dee4c2fee5
Heidi Smith <Heidi@LKs-MacBook-Pro.local>
2020-03-30 16:27:46 -0500

    dictionaries unpacking and packing

--- dictionaries.py
+++ dictionaries.py
@@ -9,3 +9,31 @@ for item in course.item():
 for key, value in course.items():
     print(key)
     print(value)
+
+# Packing with dictionaries:
+
+
+def print_teacher(**kwargs):
+    for key, value in kwargs.items():
+        print(f'{key}: {value}')
+
+
+print_teacher(name='Ashley', job='Instructor', topic='Python')
+
+
+# Unpacking with Dictionaries:
+
+teacher = {
+    'name': 'Ashley',
+    'job': 'Instructor',
+    'topic': 'Python'
+}
+
+
+def print_teacher(name, job, topic):
+    print(name)
+    print(job)
+    print(topic)
+
+
+print_teacher(**teacher)
